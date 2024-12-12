2b42bdb046626fe48808c8f4fa4f6f599962ae88
Andreas Laine <95121638+Messijahz@users.noreply.github.com>
2024-12-12 11:28:55 +0100

    Fixed testing for the IdGenerator and for the ContactFactory without mock to start with. IdGenerator are testing for unique ID's and that the result is not empty or null.
    Contactfactory is testing if the result is not null and that the data recieved is valid. Also cheks so that the Id is not empty.

---
+++ Business.Test/Factories/ContactFactory_Tests.cs
@@ -0,0 +1,58 @@
+﻿
+using Presentation.Console.MainApp.Factories;
+using Presentation.Console.MainApp.Models;
+using Xunit;
+
+namespace Business.Test.Factories;
+
+public class ContactFactory_Tests
+{
+    [Fact]
+
+    public void Create_ShouldReturnEmptyContactRegistrationForm()
+    {
+
+        // Arrange & Act
+        var result = ContactFactory.Create();
+
+
+
+        Assert.NotNull(result);
+        Assert.IsType<ContactRegistrationForm>(result);
+    }
+
+    [Fact]
+    public void Create_ShouldReturnValidContactEntity_FromContactRegistrationForm()
+    {
+        // Arrange
+        var contactRegistrationForm = new ContactRegistrationForm
+        {
+            FirstName = "Andreas",
+            LastName = "Laine",
+            Email = "andreas.laine@domain.com",
+            PhoneNumber = "0737777777",
+            StreetAddress = "Borås 7",
+            PostalCode = "777 77",
+            City = "Borås"
+        };
+
+        //Act
+        var result = ContactFactory.Create(contactRegistrationForm);
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.IsType<ContactEntity>(result);
+        Assert.NotEmpty(result.Id);
+        Assert.Equal(contactRegistrationForm.FirstName, result.FirstName);
+        Assert.Equal(contactRegistrationForm.LastName, result.LastName);
+        Assert.Equal(contactRegistrationForm.Email, result.Email);
+        Assert.Equal(contactRegistrationForm.PhoneNumber, result.PhoneNumber);
+        Assert.Equal(contactRegistrationForm.StreetAddress, result.StreetAddress);
+        Assert.Equal(contactRegistrationForm.PostalCode, result.PostalCode);
+        Assert.Equal(contactRegistrationForm.City, result.City);
+    }
+}
+
+
+    
+
---
+++ Business.Test/Helpers/IdGenerator_Tests.cs
@@ -0,0 +1,30 @@
+﻿using Presentation.Console.MainApp.Helpers;
+
+namespace Business.Test.Helpers;
+
+public class IdGenerator_Tests
+{
+    [Fact]
+    public void GenerateUniqueId_ShouldReturnGuidAsString()
+    {
+        // Act
+        var result = UniqueIdentifierGenerator.GenerateUniqueId();
+
+        // Assert
+        Assert.True(Guid.TryParse(result, out _));
+        Assert.NotEmpty(result);
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public void GenerateUniqueId_ShouldReturnUniqueValues()
+    {
+        // Act
+        var result1 = UniqueIdentifierGenerator.GenerateUniqueId();
+        var result2 = UniqueIdentifierGenerator.GenerateUniqueId();
+
+        // Assert
+        Assert.NotEqual(result1, result2);
+    }
+}
+
