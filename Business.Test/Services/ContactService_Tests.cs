34ed57ef4b3512cf2f0a421bd8254b39685a53fc
Messijahz <andreas.laine@hotmail.com>
2025-01-06 21:53:35 +0100

    - Added mock tests for ContactService. Verified that all tests pass successfully.

--- Business.Test/Services/ContactService_Tests.cs
+++ Business.Test/Services/ContactService_Tests.cs
@@ -1,6 +1,8 @@
 ﻿using ContactListApp.Business.Data;
 using ContactListApp.Business.Models;
 using ContactListApp.Business.Services;
+using ContactListApp.Business.Interfaces;
+using Moq;
 using Xunit;
 
 namespace Business.Test.Services;
@@ -39,5 +41,76 @@ public class ContactService_Tests
 
         //Assert
         Assert.False(result);
+    }
+
+    [Fact]
+    public void Create_ShouldCallSaceListToFile_WhenContactIsValid()
+    {
+        //Arrange
+        var mockFileService = new Mock<IFileService>();
+        mockFileService.Setup(fs => fs.LoadListFromFile()).Returns(new List<ContactEntity>());
+        var contactService = new ContactService(mockFileService.Object);
+
+        var form = new ContactRegistrationForm
+        {
+            FirstName = "Andreas",
+            LastName = "Laine",
+            Email = "andreas.laine@domain.com",
+            PhoneNumber = "1234567890",
+            StreetAddress = "Borås",
+            PostalCode = "12345",
+            City = "Borås",
+        };
+
+        //Act
+        var result = contactService.Create(form);
+
+        //Assert
+        Assert.True(result);
+        mockFileService.Verify(fs => fs.SaveListToFile(It.IsAny<List<ContactEntity>>()), Times.Once);
+    }
+
+    [Fact]
+    public void Create_ShouldNotCallSaveListToFile_WhenEmailAlreadyExists()
+    {
+        //Arrange
+        var existingContact = new ContactEntity { Email = "andreas.laine@domain.com" };
+        var mockFileService = new Mock<IFileService>();
+        mockFileService.Setup(fs => fs.LoadListFromFile()).Returns(new List<ContactEntity> { existingContact });
+        var contactService = new ContactService(mockFileService.Object);
+
+        var form = new ContactRegistrationForm
+        {
+            FirstName = "Andreas",
+            LastName = "Laine",
+            Email = "andreas.laine@domain.com",
+            PhoneNumber = "1234567890",
+            StreetAddress = "Borås",
+            PostalCode = "12345",
+            City = "Borås",
+        };
+
+        //Act
+        var result = contactService.Create(form);
+
+        //Assert
+        Assert.False(result);
+        mockFileService.Verify(fs => fs.SaveListToFile(It.IsAny<List<ContactEntity>>()), Times.Never);
+    }
+
+    [Fact]
+    public void GetAll_ShouldCallLoadListFromFile()
+    {
+        //Arrange
+        var mockFileService = new Mock<IFileService>();
+        mockFileService.Setup(fs => fs.LoadListFromFile()).Returns(new List<ContactEntity>());
+        var contactService = new ContactService(mockFileService.Object);
+
+        //Act
+        var contacts = contactService.GetAll();
+
+        //Assert
+        Assert.NotNull(contacts);
+        mockFileService.Verify(fs => fs.LoadListFromFile(), Times.Once);
     }
 }
